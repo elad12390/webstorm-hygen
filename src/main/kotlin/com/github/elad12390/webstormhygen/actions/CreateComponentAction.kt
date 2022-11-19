e550e0f6f0414373771a2f3e5d8179cf42f079ec
אלעד <elad12390@gmail.com>
2022-11-19 07:28:35 +0200

    Made cmd call async because of error added support for other os's

--- src/main/kotlin/com/github/elad12390/webstormhygen/actions/CreateComponentAction.kt
+++ src/main/kotlin/com/github/elad12390/webstormhygen/actions/CreateComponentAction.kt
@@ -2,17 +2,30 @@ package com.github.elad12390.webstormhygen.actions
 
 import com.github.elad12390.webstormhygen.services.ProjectStateService
 import com.intellij.execution.configurations.GeneralCommandLine
-import com.intellij.execution.util.ExecUtil
+import com.intellij.execution.process.KillableColoredProcessHandler
+import com.intellij.execution.process.ScriptRunnerUtil
 import com.intellij.icons.AllIcons
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationType
+import com.intellij.notification.Notifications
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.fileEditor.impl.LoadTextUtil
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.guessProjectDir
 import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.util.SystemInfo
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.terminal.actions.TerminalActionUtil
+
 
 val DEFINITION_REGEX_PATTERN = "\\s*(?<DEFINITION>\\{([\\n\\s]*(type|message|name|default):.*)+[\\n\\s]*})".toRegex()
-val DEFINITION_VALUES_REGEX_PATTERN = "(type: [\"'`](?<TYPE>.*)[\"'`])|(name: [\"'`](?<NAME>.*)[\"'`])|(message: [\"'`](?<MESSAGE>.*)[\"'`])|(default: [\"'`](?<DEFAULT>.*)[\"'`])".toRegex()
+val DEFINITION_VALUES_REGEX_PATTERN =
+    "(type: [\"'`](?<TYPE>.*)[\"'`])|(name: [\"'`](?<NAME>.*)[\"'`])|(message: [\"'`](?<MESSAGE>.*)[\"'`])|(default: [\"'`](?<DEFAULT>.*)[\"'`])".toRegex()
 
 class CreateComponentAction(
     private val actionFolderName: String,
@@ -20,23 +33,56 @@ class CreateComponentAction(
 ) : AnAction("$actionFolderName ${templateFolder.name}") {
     override fun actionPerformed(e: AnActionEvent) {
         if (e.project === null) return
+
         val folderRightClicked = (e.dataContext.getData("VCS_VIRTUAL_FILES") as Iterable<VirtualFile>).first()
 
         templateFolder.findChild(actionFolderName)?.let { actionFolder ->
             val definitionFile = actionFolder.findChild("prompt.js") ?: actionFolder.findChild("index.js")
             if (definitionFile !== null) {
-                calculateArgumentMapping(e.project!!, LoadTextUtil.loadText(definitionFile), folderRightClicked)?.let { argsMapping ->
+                calculateArgumentMapping(
+                    e.project!!,
+                    LoadTextUtil.loadText(definitionFile),
+                    folderRightClicked
+                )?.let { argsMapping ->
                     // run generate in cli
+                    ProgressManager.getInstance().run(
+                        object : Task.Backgroundable(e.project!!, "Title") {
+                            override fun run(indicator: ProgressIndicator) {
+                                indicator.fraction = .1
+                                indicator.text = "Generating files with npx..."
 
-                    ExecUtil.execAndGetOutput(
-                        GeneralCommandLine(
-                            *ExecUtil.getTerminalCommand("Hygen Generator", "npx").toTypedArray(),
-                            "hygen",
-                            templateFolder.name,
-                            actionFolderName,
-                            *argsMapping.map { listOf("--${it.key}","${it.value}") }.flatten().toTypedArray()
-                        ).apply {
-                            setWorkDirectory(e.project!!.basePath)
+                                val executableName = if (SystemInfo.isWindows) {
+                                    "npx.cmd"
+                                } else {
+                                    "npx"
+                                }
+                                val output = ScriptRunnerUtil.getProcessOutput(
+                                    GeneralCommandLine(
+                                        executableName,
+                                        "hygen",
+                                        templateFolder.name,
+                                        actionFolderName,
+                                        *argsMapping.map { listOf("--${it.key}", "${it.value}") }.flatten().toTypedArray()
+                                    ).apply {
+                                        withWorkDirectory(e.project!!.basePath)
+                                        withParentEnvironmentType(GeneralCommandLine.ParentEnvironmentType.SYSTEM)
+                                    }
+                                )
+
+                                indicator.fraction = .8
+                                indicator.text = "Indexing files"
+                                project.guessProjectDir()?.refresh(true, true)
+
+                                indicator.fraction = 1.0
+                                indicator.text = "Finished"
+                                Notifications.Bus.notify(
+                                    Notification(
+                                        "Hygen Generator Group",
+                                        "Hygen Generator Finished,\n$output",
+                                        NotificationType.INFORMATION
+                                    )
+                                )
+                            }
                         }
                     )
                 }
@@ -44,7 +90,11 @@ class CreateComponentAction(
         }
     }
 
-    private fun calculateArgumentMapping(project: Project, contents: CharSequence, folderRightClicked: VirtualFile): MutableMap<String, String?>? {
+    private fun calculateArgumentMapping(
+        project: Project,
+        contents: CharSequence,
+        folderRightClicked: VirtualFile
+    ): MutableMap<String, String?>? {
         val matches = DEFINITION_REGEX_PATTERN.findAll(contents)
         val argumentMapping = mutableMapOf<String, String?>()
 
@@ -52,9 +102,16 @@ class CreateComponentAction(
             it.groups["DEFINITION"]?.value?.let { argText ->
                 val argumentDefinition = getArgumentDefinition(argText)
                 if (listOf("path", "directory", "dir").contains(argumentDefinition["NAME"]?.lowercase())) {
-                    argumentDefinition["DEFAULT"] = folderRightClicked.path.substring(ProjectStateService.instance.generateFolderPath.length)
+                    argumentDefinition["DEFAULT"] =
+                        folderRightClicked.path.substring(ProjectStateService.instance.generateFolderPath.length)
                 }
-                val inputValueOrNull = getInput(project, argumentDefinition["NAME"], argumentDefinition["TYPE"], argumentDefinition["MESSAGE"], argumentDefinition["DEFAULT"])
+                val inputValueOrNull = getInput(
+                    project,
+                    argumentDefinition["NAME"],
+                    argumentDefinition["TYPE"],
+                    argumentDefinition["MESSAGE"],
+                    argumentDefinition["DEFAULT"]
+                )
                 if (inputValueOrNull === null) return@calculateArgumentMapping null
 
                 argumentMapping[argumentDefinition["NAME"] ?: "unknown"] = inputValueOrNull
@@ -114,5 +171,4 @@ class CreateComponentAction(
             else -> null
         }
     }
-
 }
